232db1ad3c3b5e4cd9fa1bff541f6d1dc9ea3931
nadege-bourguignon <nadegepub@gmail.com>
2018-04-26 17:46:43 +0200

    assets

---
+++ Udemy ShootEmUP/Assets/Scripts/SimpleShipController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleShipController : MonoBehaviour
+{
+
+    //private Transform transformComponent;
+
+    void Awake()
+    {
+        //transformComponent = GetComponent<Transform>();
+    }
+
+	void Start () {
+		
+	}
+	
+	void Update ()
+	{
+	    float x = Input.GetAxis("Horizontal");
+	    float y = Input.GetAxis("Vertical");
+        transform.Translate(0,y,0);
+        transform.Rotate(0,0,-x);
+    }
+}
